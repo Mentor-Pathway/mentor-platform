307a3558de61f68d83374421facf2c3bbf23b884
BernardSimpson <ben.simpson@weshape.io>
2022-10-27 11:30:29 +0100

    user challenge validations completed [MP-32]

--- app/models/user_challenge.rb
+++ app/models/user_challenge.rb
@@ -1,2 +1,6 @@
 class UserChallenge < ApplicationRecord
-end
+    validates :completed, presence: true
+    validates :user_notes, presence: true, length: { maximum: 1500, }
+    validates :res_feedback, presence: true, length: { maximum: 1500, }
+    validates :rating, numericality: { less_than_or_equal_to: 5, only_integer: true }
+end         
