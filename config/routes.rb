d0aea2ddcdc36ae469eaf186a5a4cbad2b83925a
HaYeon Kim <kimhy8376@gmail.com>
2022-10-27 14:57:02 +0100

    feat: generat a controller for profile model. define show, new, create, edit, update method [MP-9]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  #This routes are created when 'profile_controller' is generated.
+  #You can delete these when you work on the routes/resources.
+  get 'profiles/show'
+  get 'profiles/new'
+  get 'profiles/create'
+  get 'profiles/edit'
+  get 'profiles/update'
+  ################################################################
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -6,4 +15,3 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "pages#home"
 end
-  
