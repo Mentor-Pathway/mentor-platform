6f56a74b75364611c6e58683315ee721b9d32c03
BsBou <borhan.boulandier@gmail.com>
2022-10-29 17:39:16 +0100

    feat: Added first and last name fields to sign up and account update views

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -266,7 +266,9 @@ Devise.setup do |config|
   # config.navigational_formats = ['*/*', :html]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
-  config.sign_out_via = :delete
+
+  # Changed to sign out by url - for testing - change back to DELETE method once logout link is present
+  config.sign_out_via = :get
 
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
