f2fda306e7a96ef2bb39b314bd9294d23722cc3c
Craig Norford <craig.norford@weshape.io>
2022-10-30 14:03:10 +0000

    feat: challenge factory[MP-3]

--- spec/models/challenge_spec.rb
+++ spec/models/challenge_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Challenge, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  title {"Beginner Task One"}
+  details { "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat." }
 end
