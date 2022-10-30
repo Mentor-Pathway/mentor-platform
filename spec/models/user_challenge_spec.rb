2fb07ca52d5fdfadafa8edb7157dad5c5436e920
Craig Norford <craig.norford@weshape.io>
2022-10-30 14:20:29 +0000

    feat: factory for user challenges [MP-3]

--- spec/models/user_challenge_spec.rb
+++ spec/models/user_challenge_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe UserChallenge, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  challenge_id { Challenge.last.id }
+  user_id { User.last.id }
 end
