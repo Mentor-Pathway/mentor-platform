8e3046f3befb0bb1abf2d9f072d37fd52c86f250
Craig Norford <craig.norford@weshape.io>
2022-10-30 16:17:07 +0000

    fix: added assoiations to profiles and user_pathways factories [MP-3]

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :profile do
     job { "Devops egineers" }
+    user { association :user }
   end
 end
--- spec/factories/user_pathways.rb
+++ spec/factories/user_pathways.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user_pathway do
     user { association :user }
+    pathway { association :pathway }
   end
 end
