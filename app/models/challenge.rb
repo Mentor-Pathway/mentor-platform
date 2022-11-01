965730bf7c001ae3329da2c7d5ba9a3abc821a84
craig norford <craig.a.norford@gmail.com>
2022-11-01 02:11:24 +0000

    feat: add one photo association to pathways, challenges and profile [MP]

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,4 +1,5 @@
 class Challenge < ApplicationRecord
     validates :title, presence: true, format: { with: /[a-zA-Z0-9_-]/}
     validates :details, presence: true
+    has_one_attached :photo
 end
--- app/models/pathway.rb
+++ app/models/pathway.rb
@@ -1,2 +1,3 @@
 class Pathway < ApplicationRecord
+  has_one_attached :photo
 end
