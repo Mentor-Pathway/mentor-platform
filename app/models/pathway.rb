32e7a07cd86821105039b884ffcf12b32ed713e5
Adrian <github@ahards.com>
2022-11-02 15:55:02 +0000

    remove comma from user model that resulted in unexpected end

--- app/models/pathway.rb
+++ app/models/pathway.rb
@@ -1,5 +1,5 @@
 class Pathway < ApplicationRecord
-    validates :title, presence: true, format: { with: /[a-zA-Z0-9_-]/}, length: { maximum: 50, }
-    validates :details, presence: true, length: { maximum: 1500, }
+    validates :title, presence: true, format: { with: /[a-zA-Z0-9_-]/}, length: { maximum: 50 }
+    validates :details, presence: true, length: { maximum: 1500 }
     validates :difficulty, presence: true, length: { minimum: 3 }
 end
