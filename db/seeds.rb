48b7f9086d3d1d15b526c4da437a13e85259be4b
HaYeon Kim <kimhy8376@gmail.com>
2022-11-03 12:40:30 +0000

    Add associations: pathway, challenge, path_challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,21 @@
 User.destroy_all
+PathChallenge.destroy_all
+Pathway.destroy_all
 Challenge.destroy_all
+
 puts "RUNNING SEED FILE"
 puts "-----------------"
 User.create!(first_name: "Ben", last_name: "Simpson", email: "ben@ben.com", password: "Testpassword1!", role: :mentor)
 puts "One user created"
 
-Pathway.create!(title: "Test 1", details: "This is a test pathway", difficulty: "Easy")
+pathway = Pathway.create!(title: "Test 1", details: "This is a test pathway", difficulty: "Easy")
 puts "One pathway created"
 
-Challenge.create!( title: "Database number 1", details: "This is a question that needs answers regarding the database", user_id: User.last.id)
+challenge = Challenge.create!( title: "Database number 1", details: "This is a question that needs answers regarding the database", user_id: User.last.id)
 puts "one challage created"
+
+PathChallenge.create!(challenge_id: challenge.id, pathway_id: pathway.id, order: 1)
+puts "one path_challenge created"
 
 
 puts "-----------------"
