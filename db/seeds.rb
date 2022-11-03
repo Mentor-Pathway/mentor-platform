fed6de07f3c449a0fcfde78d22b7fb37e3b811df
HaYeon Kim <kimhy8376@gmail.com>
2022-11-03 12:33:46 +0000

    Add pathway on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@ puts "-----------------"
 User.create!(first_name: "Ben", last_name: "Simpson", email: "ben@ben.com", password: "Testpassword1!", role: :mentor)
 puts "One user created"
 
+Pathway.create!(title: "Test 1", details: "This is a test pathway", difficulty: "Easy")
+puts "One pathway created"
+
 Challenge.create!( title: "Database number 1", details: "This is a question that needs answers regarding the database", user_id: User.last.id)
 puts "one challage created"
 
