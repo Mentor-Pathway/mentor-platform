3b991388ad431dc3b3bda6abba94105d47337e00
Craig Norford <craig.norford@weshape.io>
2022-11-05 13:52:43 +0000

    feat: add macros controller [MP-48]

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,13 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
+  config.include Devise::Test::ControllerHelpers, :type => :controller
+
+  config.include Warden::Test::Helpers
+
 end
---
+++ spec/support/macros_controller.rb
@@ -0,0 +1,16 @@
+module ControllerMacros
+    def login_admin
+      before(:each) do
+        @request.env["devise.mapping"] = Devise.mappings[:admin]
+        sign_in FactoryBot.create(:admin) # Using factory bot as an example
+      end
+    end
+  
+    def login_user
+      before(:each) do
+        @request.env["devise.mapping"] = Devise.mappings[:user]
+        user = FactoryBot.create(:user)
+        sign_in user
+      end
+    end
+  end
