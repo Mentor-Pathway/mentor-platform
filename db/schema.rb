a47c5021f6dfa2979da1e9ba0b70feadfe5365f3
HaYeon Kim <kimhy8376@gmail.com>
2022-11-06 18:50:05 +0000

    Add user reference to Pathways

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_01_125809) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_06_184532) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,6 +71,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_01_125809) do
     t.string "difficulty"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_pathways_on_user_id"
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -121,6 +123,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_01_125809) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "pathways", "users"
   add_foreign_key "user_challenges", "challenges"
   add_foreign_key "user_challenges", "users"
 end
