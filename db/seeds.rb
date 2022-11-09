e230c6c7c961e642416470de63ab14383d3cc477
BernardSimpson <ben.simpson@weshape.io>
2022-11-09 13:19:52 +0000

    added seed file [MP-29]

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,80 +5,61 @@ User.destroy_all
 puts "RUNNING SEED FILE"
 puts "-----------------"
 
-mentor = User.create!(
+@mentor = User.create!(
     first_name: "Mentor", last_name: "Test", email: "mentor@test.com", password: "Testpassword1!", role: "mentor")
 
-mentee = User.create!(
-    first_name: "Mentee", last_name: "Test", email: "mentee@test.com", password: "Testpassword1!", role: "mentee")    
+@mentee = User.create!(
+    first_name: "Mentee", last_name: "Test", email: "mentee@test.com", password: "Testpassword1!", role: "mentee")
 
-path1 = Pathway.create!(
-    title: "Cloud based questions",
-    details: "These are introductry cloud based questions. If you have completed and understood the Cloud Practioner course you should find these questions relatively easy.",
-    difficulty: "Low",
-)
+level = ["beginner", "intermidiate", "professional", "expert" ]
 
-chall1 = Challenge.create!(
-    title: "Q1. Public Cloud and Private Cloud provider",
-    details: "In this excersice I need you to research and answer the following question. What is the difference between a Private cloud provider and a Public cloud provider? Please take your time and use the internet as a resource tool.",
-    user_id: User.first.id
-)
+mark = ["1", "2", "3", "4", "5" ]
 
-PathChallenge.create!(
-    challenge_id: chall1.id,
-    pathway_id: path1.id, 
-    order: 1
-)
+def CreateChallenge(order)
+    Challenge.create!(
+        title: "Q #{order} Public Cloud and Private Cloud provider",
+        details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        user_id: @mentor.id 
+    )
 
-chall2 = Challenge.create!(
-    title: "Q2. Startups",
-    details: "Please explain in detail what advantages does the cloud offer to startups? Please take your time and use the internet as a resource tool.",
-    user_id: User.first.id 
-)
+    PathChallenge.create!(
+        challenge_id: Challenge.last.id,
+        pathway_id: Pathway.last.id, 
+        order: order  
+    )
+end
 
-PathChallenge.create!(
-    challenge_id: chall1.id,
-    pathway_id: path1.id, 
-    order: 2
-)
+20.times do |i| 
+    level_choice = rand(0..3) 
+    Pathway.create!(
+    title: "Pathway, #{i}!",
+    details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. #{i}!",
+    difficulty: level[level_choice] ,)
 
-chall3 = Challenge.create!(
-    title: "Q3. Saving cost",
-    details: "For the last question, simply explain how does the scale of cloud computing help you to save costs?",
-    user_id: User.first.id 
-)
+    20.times do |i| 
+        CreateChallenge(i)
+        
+        20.times do |i|
+            mark_choice = rand(0..4)
+            UserChallenge.create!(
+            notes: "All questions answered correctly and presented on time. #{i}!",
+            feedback: "I am impressed with the answers, only issue is I would have preferred more detail. Either way great job. #{i}!",
+            rating: mark[mark_choice] ,)
+        end 
+    end         
+end
 
-PathChallenge.create!(
-    challenge_id: chall1.id,
-    pathway_id: path1.id, 
-    order: 3
-)
-
-chall4 = Challenge.create!(
-    title: "Q4. Deployment models",
-    details: "There are three cloud computing models. Can you please name the three different models?",
-    user_id: User.first.id 
-)
-
-PathChallenge.create!(
-    challenge_id: chall1.id,
-    pathway_id: path1.id, 
-    order: 4
-)
-
-UserChallenge.create!(
-    notes: "All questions answered correctly and presented on time.",
-    feedback: "I am impressed with the answers, only issue is I would have preferred more detail. Either way great job",
-    rating: "4",
-    user_id: User.first.id
-)
 
 
 puts "created #{User.count} users"
 puts "created #{Pathway.count} pathways"
 puts "Created #{Challenge.count} challenges"
-puts "Created #{PathChallenge.count} Path Challenges"
-
-
+puts "Created #{PathChallenge.count} Path challenges"
+puts "created #{UserChallenge.count} User challenges"
 
 puts "-----------------"
 puts "SEED FINISHED"
