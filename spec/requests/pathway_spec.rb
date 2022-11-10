7f43dda5e6a2eab1a8b7da162d07d479c2be4fd0
craig norford <craig.a.norford@gmail.com>
2022-11-10 19:01:41 +0000

    fix: adjust test for creator ids on pathways[MP-48]

--- spec/requests/pathway_spec.rb
+++ spec/requests/pathway_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "Pathways", type: :request do
   describe "GET /edit" do
-    
+
 
     context "mentor can access" do
       it "should return 200:OK" do
         mentor = create(:user, role: "mentor")
-        pathway = create(:pathway)
+        pathway = create(:pathway, user: mentor)
         login_as mentor
         get edit_pathway_path(pathway)
         expect(response).to have_http_status(:success)
@@ -17,7 +17,7 @@ RSpec.describe "Pathways", type: :request do
     context "mentee cannot access" do
       it "should redirect ro root" do
         mentee = create(:user, role: "mentee")
-        pathway = create(:pathway)
+        pathway = create(:pathway, user: mentee)
         login_as mentee
         get edit_pathway_path(pathway)
         expect(response).to redirect_to(root_path)
