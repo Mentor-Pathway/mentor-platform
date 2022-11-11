200185794f380104cded4a46b9d66db76229e51e
BernardSimpson <ben.simpson@weshape.io>
2022-11-11 09:26:18 +0000

    updated seed file with loops

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,12 +33,13 @@ end
 
 5.times do |i| 
     level_choice = rand(0..3) 
-    Pathway.create!(
+    Pathway.create!( user_id: @mentor.id,
     title: "Pathway, #{i}!",
     details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
     Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. #{i}!",
-    difficulty: level[level_choice] ,)
+    difficulty: level[level_choice] )
+     
 
     20.times do |i| 
         CreateChallenge(i)
