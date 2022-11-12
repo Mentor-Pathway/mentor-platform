8e829b22bbce55114e31ea2f27b1da62a44fec07
Adrian <github@ahards.com>
2022-11-12 17:42:23 +0000

    fix: remove length for difficult as not needed; Rails does not allow incorrect enum assignment [MP-60]

--- app/models/pathway.rb
+++ app/models/pathway.rb
@@ -1,7 +1,7 @@
 class Pathway < ApplicationRecord
   validates :title, presence: true, format: {with: /[a-zA-Z0-9_-]/}, length: {maximum: 50}
   validates :details, presence: true, length: {maximum: 1500}
-  validates :difficulty, presence: true, length: {minimum: 3}
+  validates :difficulty, presence: true
 
   belongs_to :user
   has_many :path_challenges
