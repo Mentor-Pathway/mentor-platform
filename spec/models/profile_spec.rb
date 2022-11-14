f858cc2bafa2b8af71b93e607385cb375e885d07
BsBou <borhan.boulandier@gmail.com>
2022-11-14 23:02:31 +0000

    feat: added tests for bio and job title MP-90

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -6,16 +6,21 @@ RSpec.describe Profile, type: :model do
       profile = (build(:profile))
       expect(profile.user).to be_valid
     end
-  end
-  describe "Validations" do
-    it "Has a valid linkedin url" do
-      profile = (build(:profile))
-      expect(profile.linkedin).to be_a_kind_of String 
-    end
-    it "Has a valid github url" do
-      profile = (build(:profile))
-      expect(profile.github).to be_a_kind_of String 
-    end
+    it "has a profile picture"
   end
 
+  describe "Validations" do
+    it "Has a valid linkedin url" do
+      expect { create(:profile, linkedin: 'Boris Johnson') }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+    it "Has a valid github url" do
+      expect { create(:profile, github: 'torvalds') }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+    it "Bio has a minimum of 6 words" do
+      expect { create(:profile, bio: 'Ever since I') }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+    it "Job title contains no special characters" do
+      expect { create(:profile, job: '&*^%$') }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
 end
