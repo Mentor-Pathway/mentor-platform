c33e6e27cd625dbf67ac0b6cb3a05968ec1874e4
BsBou <borhan.boulandier@gmail.com>
2022-11-14 16:49:10 +0000

    feat: Added bio to profile model MP-56

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_14_164637) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_14_164828) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -82,6 +82,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_14_164637) do
     t.datetime "updated_at", null: false
     t.string "linkedin"
     t.string "github"
+    t.text "bio"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
