77adc2f7a53452e6b208d63d88e60885c8fda484
BsBou <borhan.boulandier@gmail.com>
2022-11-14 17:40:25 +0000

    feat: added first basic tests for profile model

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,4 +1,21 @@
 require "rails_helper"
 
 RSpec.describe Profile, type: :model do
+  describe "Associations" do
+    it "A profile must belong to a user" do
+      profile = (build(:profile))
+      expect(profile.user).to be_valid
+    end
+  end
+  describe "Validations" do
+    it "Has a valid linkedin url" do
+      profile = (build(:profile))
+      expect(profile.linkedin).to be_a_kind_of String 
+    end
+    it "Has a valid github url" do
+      profile = (build(:profile))
+      expect(profile.github).to be_a_kind_of String 
+    end
+  end
+
 end
