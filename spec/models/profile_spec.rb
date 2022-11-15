1bca132c6cbf4cbe81326ea52cf15ad2b82baf7d
BsBou <borhan.boulandier@gmail.com>
2022-11-15 20:33:21 +0000

    feat: added tests for profile photo association

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,12 +1,21 @@
 require "rails_helper"
+require "open-uri"
 
 RSpec.describe Profile, type: :model do
   describe "Associations" do
-    it "A profile must belong to a user" do
+
+    it "must belong to a user" do
       profile = (build(:profile))
       expect(profile.user).to be_valid
     end
-    it "has a profile picture"
+    
+    it "has a profile picture" do
+      profile = (build(:profile))
+      file = URI.open("https://images.unsplash.com/photo-1613679074971-91fc27180061?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80")
+      profile.photo.attach(io: file, filename: "avatar.jpg")
+      profile.save
+      expect(profile.photo.attached?).to be_truthy
+    end
   end
 
   describe "Validations" do
