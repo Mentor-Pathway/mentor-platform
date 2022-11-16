13e8f8699b7947a45cd590ab07f92c32a01e5041
BsBou <borhan.boulandier@gmail.com>
2022-11-16 15:41:27 +0000

    feat: added profile to mentee in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,13 @@ puts "-----------------"
 @mentee = User.create!(
     first_name: "Mentee", last_name: "Test", email: "mentee@test.com", password: "Testpassword1!", role: "mentee")
 
+@mentee.profile.update!(
+    job: 'Devops Enginner',
+    bio: "Ever since I was a little child, I've dreamt of being a DevOps Engineer. I'm still dreaming!",
+    linkedin: "linkedin.com/in/boris-johnson",
+    github: 'www.github.com/torvalds'
+)
+
 level = ["beginner", "intermediate", "advanced", ]
 
 def CreateChallenge(i)
