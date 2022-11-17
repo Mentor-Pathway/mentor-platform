0b54c0ff33f9c72a57b102ef15158cee61848e36
BsBou <borhan.boulandier@gmail.com>
2022-11-17 12:31:44 +0000

    feat: test to check profile edit authorization

---
+++ spec/requests/profile_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "Profiles", type: :request do
+  describe "GET /edit" do
+
+    context "profile owner can access" do
+      it "should return 200:OK" do
+        profile = create(:profile)
+        login_as profile.user
+        get edit_profile_path(profile)
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "not profile owner cannot access" do
+      it "should not return 200:OK" do
+        profile = create(:profile)
+        user = create(:user)
+        login_as user
+        get edit_profile_path(profile.)
+        expect(response).to_not have_http_status(:success)
+      end
+    end
+  end
+end
