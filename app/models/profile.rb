bba326c1a554bbb7bc1a3d1f3134551a5a5dea94
BsBou <borhan.boulandier@gmail.com>
2022-11-17 16:32:59 +0000

    fix: added comment

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,6 @@
 class Profile < ApplicationRecord
+  # On initial creation, profile attributes are nil, on profile update, profile attributes are changed 
+  # from nil to "", both are valid 
   validates :job, format: {with: /\A[a-zA-Z0-9 ]+\z/}, allow_nil: true, allow_blank: true
   validates :linkedin, format: { with: /.*linkedin.\S*\/\S*/ }, allow_nil: true, allow_blank: true
   validates :github, format: { with: /.*github.\S*\/\S*/ }, allow_nil: true, allow_blank: true
