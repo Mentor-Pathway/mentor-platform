4ba26cdb2094140e99372dbc5ba274d59fbe0420
Craig Norford <craig.norford@weshape.io>
2022-11-20 23:58:46 +0000

    feat: user pathways link to show page  [MP-66]

--- app/controllers/user_pathways_controller.rb
+++ app/controllers/user_pathways_controller.rb
@@ -1,9 +1,13 @@
 class UserPathwaysController < ApplicationController
   before_action :set_user
-  before_action :set_user_pathway, only: %i[destroy approved]
+  before_action :set_user_pathway, only: %i[ show destroy approved]
 
   def new
     @user_pathway = UserPathway.new
+  end
+
+  def show
+
   end
 
   def create
