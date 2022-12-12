bfc17f6fe247138f87fdd4694e770f0526ec5c19
BsBou <borhan.boulandier@gmail.com>
2022-12-12 14:53:17 +0000

    inital tests for user_pathway

--- spec/models/user_pathway_spec.rb
+++ spec/models/user_pathway_spec.rb
@@ -1,4 +1,19 @@
 require "rails_helper"
 
 RSpec.describe UserPathway, type: :model do
+  describe 'Association' do
+    it "must belong to a user" do
+      expect { create(:user_pathway, user: nil) }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: User must exist")
+    end
+    it "must belong to a pathway" do
+      expect { create(:user_pathway, pathway: nil) }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Pathway must exist")
+    end
+    it 'has user_challenges on creation' do
+      pathway = (create(:pathway))
+      challenge = (create(:challenge))
+      path_challenge = (create(:path_challenge, pathway: pathway, challenge: challenge))
+      user_pathway = (create(:user_pathway, pathway: pathway))
+      expect(user_pathway.user_challenges).not_to be_empty
+    end    
+  end
 end
