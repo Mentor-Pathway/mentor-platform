1667534b32b5574c60455222a99a2cb223750ae5
BsBou <borhan.boulandier@gmail.com>
2022-12-12 15:00:06 +0000

    create user_challenges on user_pathway creation

--- app/models/user_pathway.rb
+++ app/models/user_pathway.rb
@@ -1,5 +1,18 @@
 class UserPathway < ApplicationRecord
-    belongs_to :user
-    belongs_to :pathway
-    has_many :user_challenges, dependent: :destroy
-end
+  belongs_to :user
+  belongs_to :pathway
+  has_many :user_challenges, dependent: :destroy
+  
+  after_create :creat_user_challenges
+
+  private
+  
+  def creat_user_challenges
+    self.pathway.challenges.each do |challenge|
+      UserChallenge.create!(
+        user_pathway: self,
+        challenge: challenge
+      )
+    end
+  end
+end 
