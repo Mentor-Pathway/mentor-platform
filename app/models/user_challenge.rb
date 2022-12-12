9f6e772b7c2509c6985a3583806ef659871917ef
BsBou <borhan.boulandier@gmail.com>
2022-12-12 14:54:27 +0000

    allow user_challenge to exist without rating

--- app/models/user_challenge.rb
+++ app/models/user_challenge.rb
@@ -1,7 +1,7 @@
 class UserChallenge < ApplicationRecord
   validates :notes, length: {maximum: 1500}
   # validates :feedback, length: {maximum: 1500}
-  validates :rating, numericality: {less_than_or_equal_to: 5, only_integer: true, greater_than_or_equal_to: 1}
+  validates :rating, numericality: {less_than_or_equal_to: 5, only_integer: true, greater_than_or_equal_to: 1}, allow_nil: true
   has_many :comments, dependent: :destroy
   belongs_to :user_pathway
   belongs_to :challenge
