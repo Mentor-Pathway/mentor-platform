3d8009b970a36d255de09b8d9791e8da14c8b6d8
HaYeon Kim <kimhy8376@gmail.com>
2023-01-05 23:39:28 +0000

    task: change 'note' css

--- app/controllers/user_challenges_controller.rb
+++ app/controllers/user_challenges_controller.rb
@@ -3,16 +3,11 @@ class UserChallengesController < ApplicationController
   before_action :set_user_challenge, only: %i[edit update show]
 
   def show
-    @user_challenge = current_user.user_challenges.find(params[:id])
     @notes = @user_challenge.notes.reverse
   end
 
   def new
     @user_challenge = UserChallenge.new
-  end
-
-  def show
-
   end
 
   def create
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,76 +1,80 @@
-Pathway.destroy_all
-Challenge.destroy_all
 UserChallenge.destroy_all
-User.destroy_all
 UserPathway.destroy_all
+Challenge.destroy_all
+Pathway.destroy_all
+User.destroy_all
 puts "RUNNING SEED FILE"
 puts "-----------------"
 
 @mentor = User.create!(
-    first_name: "Mentor", last_name: "Test", email: "mentor@test.com", password: "Testpassword1!", role: "mentor")
+  first_name: "Mentor", last_name: "Test", email: "mentor@test.com", password: "Testpassword1!", role: "mentor")
 
 @mentee = User.create!(
-    first_name: "Mentee", last_name: "Test", email: "mentee@test.com", password: "Testpassword1!", role: "mentee")
+  first_name: "Mentee", last_name: "Test", email: "mentee@test.com", password: "Testpassword1!", role: "mentee")
 
 User.create!(
-    first_name: "Mentor", last_name: "Fresh", email: "mentor@fresh.com", password: "Testpassword1!", role: "mentor")
+  first_name: "Mentor", last_name: "Fresh", email: "mentor@fresh.com", password: "Testpassword1!", role: "mentor")
 
-    User.create!(
-    first_name: "Mentee", last_name: "Fresh", email: "mentee@fresh.com", password: "Testpassword1!", role: "mentor")
+User.create!(
+  first_name: "Mentee", last_name: "Fresh", email: "mentee@fresh.com", password: "Testpassword1!", role: "mentor")
 
 @mentee.profile.update!(
-    job: 'Devops Enginner',
-    bio: "Ever since I was a little child, I've dreamt of being a DevOps Engineer. I'm still dreaming!",
-    linkedin: "linkedin.com/in/boris-johnson",
-    github: 'www.github.com/torvalds'
+  job: 'Devops Enginner',
+  bio: "Ever since I was a little child, I've dreamt of being a DevOps Engineer. I'm still dreaming!",
+  linkedin: "linkedin.com/in/boris-johnson",
+  github: 'www.github.com/torvalds'
 )
 
 level = ["beginner", "intermediate", "advanced", ]
 
 def CreateChallenge(i)
-    Challenge.create!(
-        title: "Challenge #{i + 1}!",
-        details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        user_id: @mentor.id
-    )
+  Challenge.create!(
+    title: "Challenge #{i + 1}!",
+    details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    user_id: @mentor.id
+  )
 
-    PathChallenge.create!(
-        challenge_id: Challenge.last.id,
-        pathway_id: Pathway.last.id,
-        order: (i)
-    )
+  PathChallenge.create!(
+    challenge_id: Challenge.last.id,
+    pathway_id: Pathway.last.id,
+    order: (i)
+  )
 end
 
 5.times do |i|
-    level_choice = rand(0..2)
-    Pathway.create!( user_id: @mentor.id,
+  level_choice = rand(0..2)
+  Pathway.create!(
+    user_id: @mentor.id,
     title: "Pathways, #{i + 1}!",
     details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
     Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. #{i + 1}!",
-    difficulty: level[level_choice] )
+    difficulty: level[level_choice]
+  )
 
-    20.times do |i|
-        CreateChallenge(i)
-    end
+  20.times do |i|
+    CreateChallenge(i)
+  end
 end
 
-UserPathway.create!(user: @mentee, pathway: Pathway.last)
+puts "Create User Pathway"
+user_pathway = UserPathway.create!(user: @mentee, pathway: Pathway.last)
 
-
-puts "Create User Challenge object"
-UserChallenge.create!(
-  user_id: @mentee.id, challenge_id: Challenge.all.sample.id, rating: 5, user_pathway_id: UserPathway.first.id
-)
+puts "Update User Challenge objects"
+UserChallenge.all.each do |challenge|
+  UserChallenge.update!(
+    user_id: @mentee.id, rating: 5, completed: false
+  )
+end
 
 puts "created #{User.count} users"
 puts "created #{Pathway.count} pathways"
 puts "Created #{Challenge.count} challenges"
 puts "Created #{PathChallenge.count} Path challenges"
-puts "Created #{UserChallenge.count} User challenges"
 puts "Created #{UserPathway.count} User Pathways"
+puts "Created #{UserChallenge.count} User challenges"
 
 puts "-----------------"
 puts "SEED FINISHED"
