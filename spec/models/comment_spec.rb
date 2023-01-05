982e730b2eb30287b79c295e0b67d997948ccb31
Adrian Hards <github@ahards.com>
2023-01-05 15:27:29 +0000

    add basic validations and associations in spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,13 @@
 require "rails_helper"
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_length_of(:message).is_at_most(1500) }
+    it { should validate_presence_of(:message) }
+  end
+
+  describe "associations" do
+    it { should belong_to(:user_challenge) }
+    it { should belong_to(:user) }
+  end
 end
