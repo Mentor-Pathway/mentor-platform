fff0ae2bdfdfc90b6a52c035199dbab88ff45231
Adrian Hards <github@ahards.com>
2023-01-05 15:29:35 +0000

    change from should to expected to

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,12 +2,12 @@ require "rails_helper"
 
 RSpec.describe Comment, type: :model do
   describe "validations" do
-    it { should validate_length_of(:message).is_at_most(1500) }
-    it { should validate_presence_of(:message) }
+    it { is_expected.to validate_length_of(:message).is_at_most(1500) }
+    it { is_expected.to validate_presence_of(:message) }
   end
 
   describe "associations" do
-    it { should belong_to(:user_challenge) }
-    it { should belong_to(:user) }
+    it { is_expected.to belong_to(:user_challenge) }
+    it { is_expected.to belong_to(:user) }
   end
 end
