fa1ec1be2beab7994331e3a9b45473b72c42d457
HaYeon Kim <kimhy8376@gmail.com>
2023-01-06 00:30:06 +0000

    fix: fix user_challenge rspec testing; no need to validate notes length

--- spec/models/user_challenge_spec.rb
+++ spec/models/user_challenge_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe UserChallenge, type: :model do
   let(:user_challenge) { create(:user_challenge) }
 
   describe "validations" do
-    it { is_expected.to validate_length_of(:notes).is_at_most(1500) }
+   #  it { is_expected.to validate_length_of(:notes).is_at_most(1500) }
     it { is_expected.to validate_numericality_of(:rating).is_less_than_or_equal_to(5).only_integer.is_greater_than_or_equal_to(1) }
     it { is_expected.to allow_value(nil).for(:rating) }
   end
