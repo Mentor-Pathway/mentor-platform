7a3650d26517ca4331ff9a5a7622299ac04207b5
HaYeon Kim <kimhy8376@gmail.com>
2023-01-06 00:27:13 +0000

    fix: fix rspec testing

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -3,17 +3,7 @@ require 'rails_helper'
 RSpec.describe Note, type: :model do
   let(:note) { create(:note) }
 
-  describe "Associations:" do
+  describe "associations" do
     it { is_expected.to belong_to(:user_challenge) }
-  end
-
-  describe "CRUD" do
-    it "note must have user_challenge_id" do
-      expect(note).to be_valid
-    end
-
-    it "note can be destroyed" do
-      expect { note.destroy }.to change(described_class, :count).by(-1)
-    end
   end
 end
