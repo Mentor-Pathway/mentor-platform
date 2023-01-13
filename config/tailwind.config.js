5437774a593db5b0277d113c529dbc52419f4169
BernardSimpson <ben.simpson@weshape.io>
2023-01-13 16:18:54 +0000

    [MP-115] changed nav bar design to match Diandra's figma

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -8,33 +8,4 @@ module.exports = {
     "./app/javascript/**/*.js",
     "./app/views/**/*.{erb,haml,html,slim}",
   ],
-
-  theme: {
-    extend: {
-      backgroundcolor: {
-        'primary': '#3182ce',
-        'primary-dark': {
-          light: '#2c5282',
-          dark: '#0c2c54'
-        },
-      },
-
-      variants: {
-        backgroundColor: ['dark', 'dark-hover', 'dark-group-hover'],
-        textColor: ['dark', 'dark-hover', 'dark-active'],
-        borderColor: ['dark', 'dark-focus', 'dark-focus-within'],
-      },
-
-      fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-      },
-
-      plugins: [
-        require("@tailwindcss/forms"),
-        require("@tailwindcss/aspect-ratio"),
-        require("@tailwindcss/typography"),
-      ],
-
-    },
-  },
 };
