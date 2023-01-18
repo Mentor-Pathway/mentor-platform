7d9611203d28d5ba8c0dd4e43c966baf7ad5a629
BsBou <borhan.boulandier@gmail.com>
2023-01-18 09:37:57 +0000

    created tags table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_09_163243) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_18_093443) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -100,6 +100,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_09_163243) do
     t.string "github"
     t.text "bio"
     t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "user_challenges", force: :cascade do |t|
