9047a5a6009ae61244bd65de10665681b9500c81
SafiyahZ <safiyahzaman@gmail.com>
2023-01-19 09:39:46 +0000

    Pathway Index half styled

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,10 +51,8 @@ end
   level_choice = rand(0..2)
   Pathway.create!(
     user_id: @mentor.id,
-    title: "Pathways, #{i + 1}!",
-    details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. #{i + 1}!",
+    title: "Pathway #{i + 1}!",
+    details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. #{i + 1}!",
     difficulty: level[level_choice]
   )
 
