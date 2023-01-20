f62af42cb8fda566f6ed5b2e5aca3ffdb4cad212
Adrian Hards <github@ahards.com>
2023-01-20 20:43:09 +0000

    fix: only show pathways for selected difficulty

--- app/javascript/controllers/pathway_controller.js
+++ app/javascript/controllers/pathway_controller.js
@@ -2,14 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="pathway"
 export default class extends Controller {
+   static targets = ["path"]
+
    connect() {
       console.log("connected to the path stimulus controller")
     }
 
    change(event) {
-      var selectedDifficulty = event.target.value
-      this.pathwayTargets.forEach(pathway => {
-         if (pathway.getAttribute("data-difficulty") === selectedDifficulty) {
+      const selectedDifficulty = (event.target.value).toLowerCase();
+      this.pathTargets.forEach(pathway => {
+         if (pathway.attributes.difficulty.value === selectedDifficulty) {
             pathway.style.display = "block"
          } else {
             pathway.style.display = "none"
