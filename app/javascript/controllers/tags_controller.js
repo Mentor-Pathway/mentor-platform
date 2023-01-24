229da973ce13e259921435632b311797e1b6ceb1
SafiyahZ <safiyahzaman@gmail.com>
2023-01-24 08:47:18 +0000

    Pathway Index Page: color of <hr> line made visible [MP-130]

--- app/javascript/controllers/tags_controller.js
+++ app/javascript/controllers/tags_controller.js
@@ -2,17 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="tags"
 export default class extends Controller {
-   static targets = ["path"]
+   static targets = ["tag"]
 
 }
 
-document.addEventListener("onclick", (event) => {
-   var button = event.target;
-   if (button.classList.contains("tag-button")) {
-      var tag = button.innerText.toLowerCase();
-      var pathways = document.querySelectorAll(`[data-tag='${tag}']`);
-      var allpathways = document.querySelectorAll('.pathway');
-      allpathways.forEach(pathway => pathway.style.display = 'none');
-      pathways.forEach(pathway => pathway.style.display = 'block');
-   }
-});
