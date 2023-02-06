9821721ca66a306a44a78071c7a818bdb685173d
craig norford <craig.a.norford@gmail.com>
2023-02-06 06:26:46 +0000

    feat: github athentication [MP-152]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {omniauth_callbacks: "callbacks"}
 
   devise_scope :user do
     get "/users/sign_out" => "devise/sessions#destroy"
